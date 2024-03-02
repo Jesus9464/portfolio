4be0bba19af5f23f32d2d78a51e9e66555239427
Jesus Galvis <74638215+Jesus9464@users.noreply.github.com>
2024-03-01 20:54:30 -0500

    feat: add background complete

---
+++ src/components/BackGround.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import '@styles/background.css';
+
+const BackGround = () => {
+	return (
+		<>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+			<div className="firefly"></div>
+		</>
+	);
+};
+
+export default BackGround;
